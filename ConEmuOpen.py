b1b95f48c7eff98c913255f86fa7ef30d6edb721
kiliwalk <kiliwalk@qq.com>
2015-09-29 13:32:25 +0800

    open outside file in project window will show the file path other than project name

--- ConEmuOpen.py
+++ ConEmuOpen.py
@@ -2,10 +2,12 @@ import os, sublime, sublime_plugin
 
 class ConEmuOpenCommand():
     def get_project(self):
+        has_project = 0
         if self.window.project_file_name(): # try project file name first
             project_dir, project_name = os.path.split(self.window.project_file_name())
             project_name, ext = os.path.splitext('st:' + project_name)
             project_dir = self.window.folders()[0]
+            has_project = 1
         elif self.window.folders(): # if no project, use the top folder
             project_dir = self.window.folders()[0]
             project_name = project_dir
@@ -14,25 +16,25 @@ class ConEmuOpenCommand():
             project_name = project_dir
         else: #then exit
             return (None, None)
-        return (project_name, project_dir)
+        return (project_name, project_dir, has_project)
 
     def open_conemu(self, dirname, title):
         command= "start conemu.exe /Single /Dir "+dirname+" /cmdlist cmd -new_console:t:\""+title+"\""
         os.system(command)
         
 # open project folder
-class ProjectCommand(sublime_plugin.WindowCommand, ConEmuOpenCommand):
+class OpenConemuProjectCommand(sublime_plugin.WindowCommand, ConEmuOpenCommand):
     def run(self):
-        project_name, project_dir = self.get_project()
+        project_name, project_dir, has_project = self.get_project()
         if not project_name:
             return
             
         self.open_conemu(project_dir, project_name)
         
 # open "here" folder
-class HereCommand(sublime_plugin.WindowCommand, ConEmuOpenCommand):
+class OpenConemuHereCommand(sublime_plugin.WindowCommand, ConEmuOpenCommand):
     def run(self, paths=[]):
-        project_name, project_dir = self.get_project()
+        project_name, project_dir, has_project = self.get_project()
         if not project_name:
             return
         
@@ -41,15 +43,19 @@ class HereCommand(sublime_plugin.WindowCommand, ConEmuOpenCommand):
             if os.path.isfile(heredir):
                 heredir = os.path.dirname(heredir)
         elif self.window.active_view().file_name(): 
-            heredir, _ = os.path.split(self.window.active_view().file_name())
+            heredir = os.path.dirname(self.window.active_view().file_name())
         else: # if no active file open, then try to open project folder
             heredir = project_dir
         
-        # get the relative path from the project dir
-        rel_path = os.path.relpath(heredir, project_dir)
-        if rel_path == '.':
-            title = project_name
-        else: 
-            title = project_name + ': ' + rel_path
+        # get the tab title
+        title = heredir
+        if has_project: 
+            rel_path = os.path.relpath(heredir, project_dir)
+            if rel_path == '.':
+                title = project_name
+            elif rel_path[:2] == '..': # not in project, show the whole path
+                title = heredir
+            else: 
+                title = project_name + ': ' + rel_path
         
         self.open_conemu(heredir, title)
